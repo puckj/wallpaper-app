08dde18489766e79bb6e824a7af4f05c516ed5a4
puckj <pongsapuck.j@gmail.com>
2023-07-16 13:08:21 +0700

    Download image function

--- src/screens/ItemScreen.tsx
+++ src/screens/ItemScreen.tsx
@@ -15,6 +15,9 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { Ionicons } from "@expo/vector-icons";
 import { BlurView } from "expo-blur";
 import { getItemById, urlFor } from "../services/sanity";
+import * as FileSystem from "expo-file-system";
+import * as MediaLibrary from "expo-media-library";
+import moment from "moment";
 
 const ItemScreen = () => {
   const { params } = useRoute<ItemScreenRouteProp>();
@@ -23,7 +26,7 @@ const ItemScreen = () => {
   const [item, setItem] = useState<any>(null);
 
   useEffect(() => {
-    console.log(params, "params");
+    // console.log(params, "params");
     getItemById(params.itemId)
       .then((data) => {
         // console.log(data);
@@ -36,6 +39,44 @@ const ItemScreen = () => {
       });
   }, []);
 
+  const handleDownload = async () => {
+    let date = moment().format("YYYYMMDDhhmmss");
+    let fileUri = FileSystem.documentDirectory + `${date}.jpg`;
+    try {
+      const res = await FileSystem.downloadAsync(
+        urlFor(item.image).url(),
+        fileUri
+      );
+      // console.log(res);
+      saveFile(res.uri);
+    } catch (err) {
+      console.log("FS Err: ", err);
+    }
+  };
+
+  const saveFile = async (fileUri: string) => {
+    try {
+      const { status } = await MediaLibrary.requestPermissionsAsync();
+      if (status === "granted") {
+        try {
+          const asset = await MediaLibrary.createAssetAsync(fileUri);
+          const album = await MediaLibrary.getAlbumAsync("Download");
+          if (album == null) {
+            await MediaLibrary.createAlbumAsync("Download", asset, false);
+          } else {
+            await MediaLibrary.addAssetsToAlbumAsync([asset], album, false);
+          }
+          alert("Download Successfully!");
+        } catch (err) {
+          console.log("Save err:", err);
+        }
+      } else if (status === "denied") {
+        alert("Please allow permissions to download");
+      }
+    } catch (err) {
+      console.log("Permission error:", err);
+    }
+  };
   return (
     <View className="flex-1 bg-[#04020d] relative">
       {isLoading ? (
@@ -81,7 +122,7 @@ const ItemScreen = () => {
                     </Text>
                   </View>
 
-                  <TouchableOpacity className="flex-2">
+                  <TouchableOpacity className="flex-2" onPress={handleDownload}>
                     <Ionicons name="cloud-download" size={26} color="white" />
                   </TouchableOpacity>
                 </BlurView>
